9ab58346f0182390b7c5d3ac7fd005c3ec990a22
Apipol Sukgler <apipol@computerlogy.com>
2018-08-13 21:15:22 +0700

    [Edited]: add timeout in test api holiday

--- src/holiday/api/api_test.go
+++ src/holiday/api/api_test.go
@@ -2,11 +2,13 @@ package api_test
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	. "holiday"
 	. "holiday/api"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/labstack/echo"
 )
@@ -17,9 +19,10 @@ func Test_GetHolidaysAvailableHandler_Input_UnitedStates_Should_Be_34_days(t *te
 	requestData := []byte(`{"countryCode":"UnitedStates"}`)
 	request := httptest.NewRequest("POST", "/api/v1/holidays", bytes.NewBuffer(requestData))
 	request.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	ctx, _ := context.WithTimeout(context.TODO(), time.Second*30)
 	recorder := httptest.NewRecorder()
 	route := SetupRoute()
-	route.ServeHTTP(recorder, request)
+	route.ServeHTTP(recorder, request.WithContext(ctx))
 	response := recorder.Result()
 	json.NewDecoder(response.Body).Decode(&actualHolidayResponse)
 	if expectedHolidays != len(actualHolidayResponse.Holiday) {
