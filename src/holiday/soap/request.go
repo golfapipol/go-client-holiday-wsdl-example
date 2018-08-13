899d7353902204f7da1beeddf2ef723688be01d5
Apipol Sukgler <apipol@computerlogy.com>
2018-08-13 20:53:37 +0700

    [Added]: add request timeout 30 sec

--- src/holiday/soap/request.go
+++ src/holiday/soap/request.go
@@ -2,15 +2,28 @@ package soap
 
 import (
 	"bytes"
+	"context"
 	"encoding/xml"
+	"fmt"
 	. "holiday"
 	"io/ioutil"
 	"net/http"
+	"time"
+
+	"github.com/labstack/echo"
 )
 
-func RequestToWDSL(request HolidayRequest) HolidayResponse {
+func RequestToWDSL(holidayRequest HolidayRequest) HolidayResponse {
 	var envelope Envelope
-	response, _ := http.Post("http://www.holidaywebservice.com/HolidayService_v2/HolidayService2.asmx?wsdl", "text/xml", bytes.NewBuffer([]byte(request.ToXML())))
+	request, _ := http.NewRequest("POST", "http://www.holidaywebservice.com/HolidayService_v2/HolidayService2.asmx?wsdl", bytes.NewBuffer([]byte(holidayRequest.ToXML())))
+	request.Header.Set(echo.HeaderContentType, echo.MIMETextXML)
+	ctx, _ := context.WithTimeout(context.TODO(), time.Second*30)
+	request = request.WithContext(ctx)
+	response, err := http.DefaultClient.Do(request.WithContext(ctx))
+	if err != nil {
+		fmt.Println(err.Error())
+		return HolidayResponse{}
+	}
 	defer response.Body.Close()
 	body, _ := ioutil.ReadAll(response.Body)
 	xml.Unmarshal(body, &envelope)
